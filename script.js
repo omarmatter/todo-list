b0780082e88d557b23e1014b07da0253853c05e3
omarmatter <omabd2001@gmail.com>
2021-07-26 10:33:06 +0300

    finish to-list �

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ btn.addEventListener("click", function () {
   const btndelete = document.createElement("button");
   const complete = document.createElement("button");
   const timeout = document.createElement("span");
+  const timnum = document.createElement("span");
   const alert = document.createElement("span");
 
   var txt = text.value;
@@ -23,7 +24,9 @@ btn.addEventListener("click", function () {
     complete.classList.add("btn-primary");
     complete.innerHTML = '<i class="fa fa-check"></i>';
     timeout.classList.add("time");
-    timeout.innerHTML = "  Time out :(" + tim + "m)";
+    timnum.innerHTML = tim;
+
+    timeout.innerHTML = "  Time out :(" + timnum.outerHTML + "m)";
 
     li.innerHTML = txt + timeout.outerHTML;
     li.append(btndelete);
@@ -32,8 +35,14 @@ btn.addEventListener("click", function () {
     list.insertBefore(li, list.childNodes[0]);
 
     setTimeout(() => {
+      const Class_name = li.className.split(" ")[0];
+      if (Class_name == "complete") return;
       timerFaild(li);
     }, tim * 60000);
+
+    setInterval(() => {
+      change(timnum.innerText, li);
+    }, 60000);
   }
 
   btndelete.addEventListener("click", function () {
@@ -41,11 +50,21 @@ btn.addEventListener("click", function () {
   });
 
   complete.addEventListener("click", function () {
-    this.parentNode.classList.add("complete");
+    console.log(this.parentNode.classList.add("complete"));
     this.classList.add("hidden");
   });
 
   function timerFaild(li) {
     console.log(li.classList.add("timeout"));
+  }
+  function change(num, li) {
+    var i = --num;
+    if (i == -1) {
+      return;
+    } else timnum.innerText = i;
+    timeout.innerHTML = "  Time out :(" + timnum.outerHTML + "m)";
+    li.innerHTML = txt + timeout.outerHTML;
+    li.append(btndelete);
+    li.append(complete);
   }
 });
